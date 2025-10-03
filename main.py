9ba5247feac3b6aafde86309c4abd1a1241431ee
shivaprasad <shivaprasadmgaikwad@gmail.com>
2025-10-03 13:42:52 +0530

    Added : Chrome and brave cache removal
    Improved : Using Rich For better color management and text rendering, Added : Logging for debugging purpose

--- main.py
+++ main.py
@@ -1,43 +1,87 @@
+import ctypes
 import os
+import subprocess
 import time
 import shutil as rm
 from platform import system
+from rich.console import Console
+from rich.prompt import Prompt
+from rich.progress import Progress
+import random
+import re
 
-import os.path as opt
 # /----------------------------------------------------------------------------
+console = Console(record=True)
+console.clear()
+
+
+# /----------------------------------------------------------------------------
+def CustomProgressbar(message: str, Time: int):
+    with Progress() as progress:
+        # Add a new task with a total of 30 steps
+        task = progress.add_task(f"[cyan]{message}[/cyan]", total=Time)
+
+        # Loop for the total number of seconds
+        while not progress.finished:
+            # Update the progress bar, advancing it by one step
+            progress.update(task, advance=1)
+            # Pause for one second to time the progress
+            time.sleep(1)
 
 
 def PrintName(text: str):
-    print("\n" + "\033[34m-" * 10 + text + "-" * 10 + "\033[0m" + "\n")
+    console.print(("\n[bold yellow]" + "- " * 5 + text + " -" * 5 + "[/bold yellow]\n"))
 
 
 def PrintException(e: Exception):
-    print(f"\033[31mcannot delete : \033[0m \n\t {e}\n")
+    console.print(
+        (f"[bold bright_red]cannot delete : [/bold bright_red] \n\t [red] {e}\n")
+    )
 
 
 def PrintSuccess(Path: str):
-    print(f"\033[32mdeleted : \033[0m \t \033[33m{Path}\033[0m \n")
+    console.print(
+        (f"[bright_green]deleted : [bright_green] \t [green]{Path}[green] \n")
+    )
+
+
+def printseprator(size: int = 25):
+    console.print(("[bold yellow]- " * size))
 
 
 def PrintFooter(name: str, sleeptimer: int):
-    print("\033[35m-" * 50)
-    print("\t" + f"App Created By : \033[32m{name.title()}\033[0m")
-    print("\033[35m-" * 50)
-    print(f"window will close in {sleeptimer} seconds \033[0m")
-    time.sleep(sleeptimer)
+    console.print("[orange1]- " * 25)
+    console.print(
+        "\t" + f"[orange1]App Created By : [bright_yellow]{name.title()}[bright_yellow]"
+    )
+    console.print("[orange1]- " * 25)
+    CustomProgressbar(
+        message=f"window will close in {sleeptimer} seconds", Time=sleeptimer
+    )
+
+
+def focus_terminal():
+    # Get the handle of the console window
+    hwnd = ctypes.windll.kernel32.GetConsoleWindow()
+    if hwnd != 0:
+        # Restore and bring to front
+        ctypes.windll.user32.ShowWindow(hwnd, 9)  # SW_RESTORE
+        ctypes.windll.user32.SetForegroundWindow(hwnd)
 
 
 # /----------------------------------------------------------------------------
 
 # R"C:\Users\user_pc_name~1\AppData\Local\Temp"
-# path_Temp = opt.expanduser(r"~\AppData\Local\Temp")
+# path_Temp = os.path.expanduser(r"~\AppData\Local\Temp")
 # path_Temp1 = r"C:\Windows\Temp"
 # path_Prefetch = r"C:\Windows\Prefetch"
 # path_SoftwareDistribution = r"C:\Windows\SoftwareDistribution\Download"
+# path_ChromeCache = os.path.expanduser(r"~\AppData\Local\Google\Chrome\User Data")
+
 
 paths: list[dict[str, str]] = [
     {
-        "path": opt.expanduser(r"~\AppData\Local\Temp"),
+        "path": os.path.expanduser(r"~\AppData\Local\Temp"),
         "message": "DELETING TEMPROARY FILES FROM (%temp%) : ",
     },
     {
@@ -54,46 +98,108 @@ paths: list[dict[str, str]] = [
     },
 ]
 
+ChromiumProfilePattern = r"^Profile\s\d+$"
+FragilePaths: list[dict[str, str]] = [
+    {
+        "path": os.path.expanduser(r"~\AppData\Local\Google\Chrome\User Data"),
+        "message": "DELETING Cache FROM (ChromeCache) : ",
+    },
+    {
+        "path": os.path.expanduser(
+            r"~\AppData\Local\BraveSoftware\Brave-Browser\User Data"
+        ),
+        "message": "DELETING Cache FROM (BravesCache) : ",
+    },
+]
+
 # /----------------------------------------------------------------------------
 
 
 def FlushDNS():
-    os.system("ipconfig /flushdns")
-    print("-" * 50)
+    subprocess.run(
+        "ipconfig /flushdns",
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+        check=True,
+    )
+    console.print("[bold indian_red1] Successfully flushed the DNS Resolver Cache.")
+    printseprator()
 
 
-# /----------------------------------------------------------------------------
+def FindSpecficFolderByREGEX(RegexPattern: str, Path: str) -> list[str] | None:
+    if os.path.exists(Path):
+        pattern = re.compile(RegexPattern)
+        folderpath = os.listdir(Path)
+        ListOfFoldersWithSameREGEXPattern = []
+        for foldername in folderpath:
+            if pattern.match(foldername):
+                ListOfFoldersWithSameREGEXPattern.append(foldername)
+        return ListOfFoldersWithSameREGEXPattern
+    else:
+        console.print()
+
+
+def DeleteBrowserCache(Path: str, message: str):
+    if os.path.exists(path=Path):
+        FoldersToDelete = [
+            "Cache",
+            "Code Cache",
+            "DawnGraphiteCache",
+            "DawnWebGPUCache",
+            "GPUCache",
+        ]
+        Listofprofiles = FindSpecficFolderByREGEX(ChromiumProfilePattern, Path) or []
+        Listofprofiles.append("Default")
+        for profilefoldername in Listofprofiles:
+            for foldersname in FoldersToDelete:
+                Delete_Temp_files(
+                    os.path.join(Path, profilefoldername, foldersname), message
+                )
 
 
 def Delete_Temp_files(path: str, message: str):
-    TempPaths = os.listdir(path)
-    PrintName(f"{message}{path}")
-    if len(TempPaths) <= 0:
-        PrintName("Nothing to clear")
-    for tempfilename in TempPaths:
-        try:
-            joinedpath = os.path.join(path, tempfilename)
-            if os.path.isdir(joinedpath):
-                rm.rmtree(joinedpath)
-                PrintSuccess(joinedpath)
-            else:
-                os.remove(joinedpath)
-                PrintSuccess(joinedpath)
+    if os.path.exists(path):
+        TempPaths = os.listdir(path)
+        PrintName(f"{message}{path}")
+        if len(TempPaths) <= 0:
+            PrintName("Nothing to clear")
+        for tempfilename in TempPaths:
+            try:
+                joinedpath = os.path.join(path, tempfilename)
+                if os.path.isdir(joinedpath):
+                    rm.rmtree(joinedpath)
+                    PrintSuccess(joinedpath)
+                else:
+                    os.remove(joinedpath)
+                    PrintSuccess(joinedpath)
 
-        except Exception as e:
-            PrintException(e)
-
-    print("-" * 150)
+            except Exception as e:
+                PrintException(e)
 
 
 # /----------------------------------------------------------------------------
-
-
 if __name__ == "__main__":
     if system() == "Windows":
         FlushDNS()
         for data in paths:
             Delete_Temp_files(path=data["path"], message=data["message"])
-        PrintFooter("shivaprasad. m. g", 30)
+        focus_terminal()
+        op = Prompt.ask(
+            case_sensitive=False,
+            choices=["Y", "N"],
+            default="Y",
+            prompt="[bold yellow]Clear Browser CACHE ? \n if yes CLOSE all BROWSERS",
+        )
+        if op == "Y":
+            for data in FragilePaths:
+                DeleteBrowserCache(data["path"], data["message"])
+
+        with open("CachePurgerLog.txt", "+w") as file:
+            file.write("".join(console.export_text()))
+
+        console.print(
+            f"[bold underline italic red] Logs have Been Genereted [/] {(random.choice(['😛', '🤧', '😝']))}"
+        )
+        PrintFooter("shivaprasad. m. g", 10)
     else:
-        print("\033[31mNOT RUNNING WINDOWS OS\033[0m")
+        console.print("[bold red]NOT RUNNING WINDOWS OS")
